ab53af9c870a11b3269bb1e0202316b56cd6b4d4
dhruvil682 <dhruvil683@gmail.com>
2023-02-04 16:01:52 +0000

    app post method

---
+++ src/App.vue
@@ -0,0 +1,504 @@
+<template>
+  <div id="app">
+    <nav class="d-flex text-light navbar bg-primary" data-bs-theme="dark">
+      Sort
+      <div class="form-check">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="sortBy"
+          v-model="selectedText"
+          checked
+          id="subject"
+          :value="'name'"
+        />
+        <label class="form-check-label" for="subject">
+          Subject
+        </label>
+      </div>
+      <div class="form-check">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="sortBy"
+          v-model="selectedText"
+          id="location"
+          :value="'location'"
+        />
+        <label class="form-check-label" for="location">
+          Location
+        </label>
+      </div>
+      <div class="form-check">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="sortBy"
+          v-model="selectedText"
+          id="price"
+          :value="'price'"
+        />
+        <label class="form-check-label" for="price">
+          Price
+        </label>
+      </div>
+      <div class="form-check">
+        <input
+          class="form-check-input"
+          type="radio"
+          name="sortBy"
+          v-model="selectedText"
+          id="availability"
+          :value="'spaces'"
+        />
+        <label class="form-check-label" for="availability">
+          Available
+        </label>
+      </div>
+      <div class="mt-3">
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="ascDec"
+            v-model="ascDec"
+            id="ascen"
+            :value="1"
+          />
+          <label class="form-check-label" for="ascen">
+            Ascending Order
+          </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="ascDec"
+            v-model="ascDec"
+            id="descen"
+            :value="2"
+          />
+          <label class="form-check-label" for="descen">
+            Descending Order
+          </label>
+        </div>
+      </div>
+    </nav>
+    <div id="app" class="flex-container">
+      <div class="content">
+        <h1>Lesson Cart</h1>
+
+        <div class="container mx-auto mt-4">
+          <div class="form-group d-flex">
+            <input
+              type="email"
+              class="form-control"
+              id="search"
+              placeholder="Search for Subject"
+              v-model="searchRESUTEXT"
+            />
+            <button class="btn btn-primary mx-1" @click="searchByText()">
+              Search
+            </button>
+          </div>
+
+          <div class="d-flex flex-wrap w-100 justify-content-between">
+            <div class=" me-1 card" v-for="(product, index) in listOfProduct">
+              <div class="card-body card-h">
+                <!-- <h5 class="card-title">{{ product.name }}</h5> -->
+                <img :src="imageSRC" class="card-img-top" :alt="imageSRC" />
+                <div
+                  class="mt-2 card-body-content d-flex flex-column justify-content-between"
+                >
+                  <div class="mt-2 ">
+                    <h6 class="card-subtitle mb-2 text-muted">
+                      <span class="text-dark"> Name Of Lesson: </span>
+                      {{ product.name }}
+                    </h6>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                      <span class="text-dark"> Space : </span>
+                      {{ product.space }}
+                    </h6>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                      <span class="text-dark">Price:</span> {{ product.price }}
+                    </h6>
+                    <h6 class="card-subtitle mb-2 text-muted">
+                      <span class="text-dark"> Place: </span
+                      >{{ product.location }}
+                    </h6>
+                  </div>
+                  <div>
+                    <button
+                      class="btn btn-success"
+                      :disabled="isBtn"
+                      @click="addToCart(product, index)"
+                    >
+                      Add to cart
+                    </button>
+                    <button
+                      class="btn btn-danger"
+                      @click="deleteProduct(product, index)"
+                    >
+                      Delete the Lesson
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h3 class="mt-3" v-if="selectedProduct.length">Shopping Cart</h3>
+        <div
+          class="d-flex flex-wrap w-100 justify-content-between"
+          v-if="selectedProduct.length"
+        >
+          <div class=" me-1 card" v-for="(product, index) in selectedProduct">
+            <div class="card-body">
+              <img :src="product.img" class="card-img-top" alt="..." />
+              <div class="mt-2 d-flex flex-column justify-content-between">
+                <div class="mt-2 ">
+                  <h6 class="card-subtitle mb-2 text-muted">
+                    User Name: {{ product.name }}
+                  </h6>
+                  <h6 class="card-subtitle mb-2 text-muted">
+                    lesson Name: {{ product.lessonName }}
+                  </h6>
+                  <h6 class="card-subtitle mb-2 text-muted">
+                    Phone Number: {{ product.phoneNumber }}
+                  </h6>
+                  <h6 class="card-subtitle mb-2 text-muted">
+                    No. of Spaces : {{ product.spaces }}
+                  </h6>
+                </div>
+                <div>
+                  <button
+                    class="btn btn-primary  "
+                    :disabled="isBtn"
+                    @click="removeFromCart(product, index)"
+                  >
+                    Remove from cart
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h3>User Details</h3>
+        <h6 class="text-danger" v-if="showMsg">
+          Please Enter your User Detail
+        </h6>
+        <form class="mt-3">
+          Name:
+          <input
+            type="text"
+            v-model="form.name"
+            class="form-group"
+            placeholder="Name"
+          />
+          Phone Number:
+          <input
+            type="number"
+            v-model="form.number"
+            class="form-group"
+            placeholder="Phone"
+          />
+          <button class="btn btn-primary">Checkout</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// import { ProductList } from "./lessons";
+
+export default {
+  name: "app",
+  data() {
+    return {
+      searchRESUTEXT: "",
+      avalaibleProducts: 5,
+      isBtn: false,
+      showMsg: false,
+      listOfProduct: [],
+      selectedProduct: [],
+      toShowSelectedProduct: [],
+      isAscDec: "asc",
+      ascDecs: 1,
+      sortSelected: "name",
+      searchValue: "",
+      form: {
+        name: "",
+        number: ""
+      },
+      imageSRC: ""
+    };
+  },
+  computed: {
+    selectedText: {
+      get() {
+        return this.sortSelected;
+      },
+      set(newValue) {
+        this.sortSelected = newValue;
+        this.sortList();
+      }
+    },
+    ascDec: {
+      get() {
+        return this.ascDecs;
+      },
+      set(newValue) {
+        this.isAscDec = newValue == 1 ? "asc" : "dsc";
+        this.sortByAscDec();
+      }
+    }
+  },
+  created() {
+    this.sortByAscDec();
+    this.getLessonsList();
+    this.image();
+  },
+  methods: {
+    image() {
+      fetch("http://localhost:3000/lesson-images", {
+        method: "GET"
+      })
+        .then(res => {
+          let data = res.json();
+          return data;
+        })
+        .then(res => {
+          this.imageSRC = res;
+          console.log(this.imageSRC);
+        });
+    },
+    searchByText() {
+      if (this.searchRESUTEXT.length) {
+        fetch(`http://localhost:3000/search`, {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ text: this.searchRESUTEXT })
+        })
+          .then(res => {
+            let data = res.json();
+            return data;
+          })
+          .then(res => {
+            this.listOfProduct = [];
+            this.listOfProduct = res;
+          });
+      } else {
+        this.getLessonsList();
+      }
+    },
+    deleteAllOrders() {
+      fetch(`http://localhost:3000/orders`, {
+        method: "DELETE",
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type"
+        }
+      })
+        .then(res => {
+          console.log("deleted successfully");
+          this.getLessonsList();
+          return res;
+        })
+        .catch(res1 => {
+          console.log("s", res1);
+        });
+    },
+    deleteProduct(product, index) {
+      fetch(`http://localhost:3000/lessons/${product._id}`, {
+        method: "DELETE",
+        data: {
+          name: "LCD",
+          topic: "new",
+          location: "new location",
+          price: 101,
+          space: 101
+        },
+        headers: {
+          "Access-Control-Allow-Headers": "Content-Type"
+        }
+      })
+        .then(res => {
+          console.log("deleted successfully");
+          this.getLessonsList();
+          return res;
+        })
+        .catch(res1 => {
+          console.log("s", res1);
+        });
+    },
+    getLessonsList() {
+      fetch("http://localhost:3000/lessons", {
+        method: "GET"
+      })
+        .then(res => {
+          let data = res.json();
+          return data;
+        })
+        .then(res => {
+          this.listOfProduct = [];
+          this.listOfProduct = res;
+          console.log(res);
+        })
+        .catch(res1 => {
+          console.log("error", res1);
+        });
+    },
+    getOrders() {
+      fetch("http://localhost:3000/orders", {
+        method: "GET"
+      })
+        .then(res => {
+          let data = res.json();
+          return data;
+        })
+        .then(res => {
+          this.selectedProduct = [];
+          this.selectedProduct = res;
+          console.log(res);
+        })
+        .catch(res1 => {
+          console.log("error", res1);
+        });
+    },
+    sortByAscDec() {
+      if (this.isAscDec == "asc") {
+        this.sortList();
+      } else {
+        this.listOfProduct = this.listOfProduct.reverse();
+      }
+    },
+    sortList() {
+      this.listOfProduct.sort((a, b) => {
+        let fa =
+          typeof a == "string"
+            ? a[this.sortSelected].toLowerCase()
+            : a[this.sortSelected];
+        let fb =
+          typeof a == "string"
+            ? b[this.sortSelected].toLowerCase()
+            : b[this.sortSelected];
+        if (fa < fb) {
+          return -1;
+        }
+        if (fa > fb) {
+          return 1;
+        }
+        return 0;
+      });
+      // this.sortByAscDec();
+    },
+    addToCart(product, index) {
+      console.log(this.form, product);
+      if (this.form.name && this.form.number) {
+        this.showMsg = false;
+        let order = {
+          name: this.form.name,
+          phoneNumber: this.form.number,
+          lessonId: product._id,
+          spaces: product.space,
+          lessonName: product.name
+        };
+        fetch("http://localhost:3000/orders", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(order)
+        })
+          .then(res => {
+            console.log(res);
+            this.getLessonsList();
+            this.getOrders();
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      } else {
+        this.showMsg = true;
+      }
+    },
+    removeFromCart(product, cart) {}
+  }
+};
+</script>
+
+<style>
+html,
+body {
+  margin: 0 !important;
+  padding: 0 !important;
+  width: 100%;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.flex-container {
+  display: -webkit-flex;
+  display: flex;
+  background-color: red;
+  min-height: 100%;
+}
+
+.sidenav {
+  background-color: #ffdddd;
+  -webkit-flex: 1;
+  flex: 1;
+}
+
+.content {
+  background-color: rgb(248, 253, 255);
+  padding: 10px;
+  -webkit-flex: 5;
+  flex: 5;
+  height: 2000px;
+}
+
+.form-check {
+  justify-content: space-between;
+  display: flex;
+  padding-left: 30px;
+}
+
+.card {
+  max-height: 250px;
+  height: 250px;
+  width: 200px;
+  margin: 5px 5px;
+}
+
+.card-body-content {
+  height: 180px;
+}
+
+.card-body {
+  padding: 0.5rem;
+}
+
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
